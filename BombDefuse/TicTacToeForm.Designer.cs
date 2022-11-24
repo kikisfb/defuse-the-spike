41d478325c7a0542bd75e2c8d1660ee75ad8a7ab
Omar Nunez Siri <omarnunezsiri@gmail.com>
2022-11-24 17:43:52 -0500

    Implement TicTacToe sub-GUI with a link back to the Main GUI

--- BombDefuse/TicTacToeForm.Designer.cs
+++ BombDefuse/TicTacToeForm.Designer.cs
@@ -28,20 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(106, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 92);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player versus AI\r\nTicTacToe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::BombDefuse.Properties.Resources._44927;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 56);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(72, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 88);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(72, 335);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 88);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(72, 438);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 88);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(200, 224);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 88);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(200, 335);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 88);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(200, 438);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(90, 88);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(331, 224);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(90, 88);
+            this.button8.TabIndex = 9;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(331, 335);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(90, 88);
+            this.button9.TabIndex = 10;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(331, 438);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(90, 88);
+            this.button10.TabIndex = 11;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button_Click);
             // 
             // TicTacToeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::BombDefuse.Properties.Resources.tic_tac_toe_seamless_background_on_dark_blue_illustration_vector;
+            this.ClientSize = new System.Drawing.Size(491, 549);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "TicTacToeForm";
             this.Text = "TicTacToeForm";
             this.Load += new System.EventHandler(this.TicTacToeForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
     }
 }
--- BombDefuse/TicTacToeForm.cs
+++ BombDefuse/TicTacToeForm.cs
@@ -12,10 +12,16 @@ namespace BombDefuse
 {
     public partial class TicTacToeForm : Form
     {
-        public TicTacToeForm()
-        {
-            InitializeComponent();
-        }
+        private TicTacToe tic;
+        private int minutes, seconds;
+        private Random random;
+        private const int MAX_MOVES = 9;
+        private List<Button> buttons;
+
+        private Key[,] board = { { Key.EMPTY, Key.EMPTY, Key.EMPTY },
+                 { Key.EMPTY, Key.EMPTY, Key.EMPTY },
+                 { Key.EMPTY, Key.EMPTY, Key.EMPTY }
+               };
 
         private Form? mainForm;
         public TicTacToeForm(Form callingForm)
@@ -23,11 +29,195 @@ namespace BombDefuse
             this.mainForm = callingForm;
 
             InitializeComponent();
+
+            random = new();
+            tic = new();
+            tic.data.SetId(1);
+
+            var list = new List<Button> { button2, button3, button4, button5, button6, button7, button8, button9, button10 };
+            buttons = list;
         }
 
         private void TicTacToeForm_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Tic is working!");
+            label1.Text = "Player versus. AI\nTicTacToe";
+            minutes = 0;
+            seconds = 0;
+            timer1.Start();
+            label2.Text = "00 minutes : 00 seconds elapsed";
+        }
+
+        private int GetIndexFromButton(Button thisButton)
+        {
+            int index = -1;
+
+            if (thisButton.Name == "button1")
+            {
+                index = 1;
+            }
+            else if(thisButton.Name == "button2")
+            {
+                index = 2;
+            }
+            else if(thisButton.Name == "button3")
+            {
+                index = 3;
+            }
+            else if(thisButton.Name == "button4")
+            {
+                index = 4;
+            }
+            else if(thisButton.Name == "button5")
+            {
+                index = 5;
+            }
+            else if(thisButton.Name == "button6")
+            {
+                index = 6;
+            }
+            else if(thisButton.Name == "button7")
+            {
+                index = 7;
+            }
+            else if(thisButton.Name == "button8")
+            {
+                index = 8;
+            }
+            else if(thisButton.Name == "button9")
+            {
+                index = 9;
+            }
+            else if(thisButton.Name == "button10")
+            {
+                index = 10;
+            }
+
+            return index;
+        }
+
+        private bool check(TicStats s)
+        {
+            Status status = s.GetStatus();
+            if(status == Status.WINNER)
+            {
+                Key k = s.GetKey();
+                if(k == Key.PLAYER)
+                {
+                    MessageBox.Show("Congratulations! You have beaten the AI. Click Ok to go back to the main menu.");
+                    closeForm();
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Oh no! The AI has beaten you. Click OK to try again.");
+                    reset();
+                }
+            }
+            else if(status == Status.STUCK)
+            {
+                MessageBox.Show("Oops! The game got stuck.");
+                reset();
+            }
+
+            return false;
+        }
+
+        private void reset()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    board[i, j] = Key.EMPTY;
+                }
+            }
+
+            foreach (Button button in buttons)
+            {
+                button.ResetText();
+                button.Enabled = true;
+                button.BackColor = DefaultBackColor;
+            }
+        }
+
+        private void closeForm()
+        {
+            mainForm.Show();
+
+            // Call the main form load to pass PuzzleData
+
+            this.Close();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button thisButton = (Button)sender;
+            int index = GetIndexFromButton(thisButton) - 2;
+
+            if(index == -1)
+            {
+                closeForm();
+            }
+            else
+            {
+                if (tic.PlayerMove(board, index, Key.PLAYER))
+                {
+                    thisButton.Text = "X";
+                    thisButton.BackColor = Color.LightBlue;
+                    thisButton.Enabled = false;
+                    if (check(tic.stats))
+                        return;
+                    
+                    int currRandom;
+
+                    do
+                    {
+                        currRandom = random.Next(0, MAX_MOVES);
+                    } while (!tic.PlayerMove(board, currRandom, Key.AI));
+                    buttons[currRandom].Text = "O";
+                    buttons[currRandom].BackColor = Color.LightCoral;
+                    buttons[currRandom].Enabled = false;
+
+                    if (check(tic.stats))
+                        return;
+                }
+                else
+                    MessageBox.Show("ERROR! That position is already occupied. Try a different one.");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string displayMinutes;
+            string displaySeconds;
+
+            if(seconds < 60)
+            {
+                seconds++;
+            }
+            else
+            {
+                seconds = 0;
+                minutes++;
+            }
+
+            if (seconds < 10)
+            {
+                displaySeconds = $"{0}{seconds}";
+            }
+            else
+                displaySeconds = $"{seconds}";
+
+            if(minutes < 10)
+            {
+                displayMinutes = $"{0}{minutes}";
+            }
+            else
+            {
+                displayMinutes = $"{minutes}";
+            }
+
+            label2.Text = $"{displayMinutes} minutes : {displaySeconds} seconds elapsed";
         }
     }
 }
