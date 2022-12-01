bee9cdbe9d268cd3813b75a54a8728b3ecb9d733
Omar Nunez Siri <omarnunezsiri@gmail.com>
2022-12-01 17:49:18 -0500

    Implement enhancement #7 with user involvement

--- BombDefuse/GuessTheFlagForm.Designer.cs
+++ BombDefuse/GuessTheFlagForm.Designer.cs
@@ -36,6 +36,7 @@
             this.check = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,10 +44,9 @@
             // 
             this.goBack.BackgroundImage = global::BombDefuse.Properties.Resources._44927;
             this.goBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.goBack.Location = new System.Drawing.Point(12, 11);
-            this.goBack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.goBack.Location = new System.Drawing.Point(14, 15);
             this.goBack.Name = "goBack";
-            this.goBack.Size = new System.Drawing.Size(66, 52);
+            this.goBack.Size = new System.Drawing.Size(75, 69);
             this.goBack.TabIndex = 3;
             this.goBack.UseVisualStyleBackColor = true;
             this.goBack.Click += new System.EventHandler(this.button_Click);
@@ -57,29 +57,30 @@
             this.label1.BackColor = System.Drawing.Color.Green;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.Ivory;
-            this.label1.Location = new System.Drawing.Point(147, 16);
+            this.label1.Location = new System.Drawing.Point(168, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(491, 47);
+            this.label1.Size = new System.Drawing.Size(608, 59);
             this.label1.TabIndex = 4;
             this.label1.Text = "Guess the country of the flag";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(428, 143);
+            this.textBox1.Location = new System.Drawing.Point(489, 191);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(237, 75);
+            this.textBox1.Size = new System.Drawing.Size(270, 99);
             this.textBox1.TabIndex = 5;
-            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(EnterKeyPressed);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EnterKeyPressed);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(269, 76);
+            this.label2.Location = new System.Drawing.Point(307, 101);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(243, 23);
+            this.label2.Size = new System.Drawing.Size(310, 28);
             this.label2.TabIndex = 6;
             this.label2.Text = "00 minutes : 00 seconds elapsed";
             // 
@@ -87,9 +88,10 @@
             // 
             this.check.BackColor = System.Drawing.Color.LightGreen;
             this.check.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.check.Location = new System.Drawing.Point(488, 233);
+            this.check.Location = new System.Drawing.Point(558, 311);
+            this.check.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.check.Name = "check";
-            this.check.Size = new System.Drawing.Size(107, 42);
+            this.check.Size = new System.Drawing.Size(122, 56);
             this.check.TabIndex = 7;
             this.check.Text = "Check";
             this.check.UseVisualStyleBackColor = false;
@@ -98,9 +100,10 @@
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(98, 119);
+            this.pictureBox1.Location = new System.Drawing.Point(112, 159);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(240, 156);
+            this.pictureBox1.Size = new System.Drawing.Size(274, 208);
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.WaitOnLoad = true;
@@ -110,20 +113,30 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(489, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 28);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Enter your guess below";
+            // 
             // GuessTheFlagForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::BombDefuse.Properties.Resources.maxresdefault;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(764, 296);
+            this.ClientSize = new System.Drawing.Size(873, 395);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.check);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.goBack);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "GuessTheFlagForm";
             this.Text = "GuessTheFlagForm";
             this.Load += new System.EventHandler(this.GuessTheFlagForm_Load);
@@ -142,5 +155,6 @@
         private Button check;
         private PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
+        private Label label3;
     }
 }
